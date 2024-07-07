f2178a6a5f34164cbce38a0b16bdcb5b4975efa3
Xingju Nie <tivvvvffffq@gmail.com>
2024-07-07 18:46:02 +0800

    本地服务注册器

--- provider/src/main/java/com/nxj/provider/ProviderExample.java
+++ provider/src/main/java/com/nxj/provider/ProviderExample.java
@@ -1,5 +1,7 @@
 package com.nxj.provider;
 
+import com.nxj.common.service.UserService;
+import com.nxj.registry.LocalRegistry;
 import com.nxj.server.HttpServer;
 import com.nxj.server.VertxHttpServer;
 
@@ -8,6 +10,11 @@ import com.nxj.server.VertxHttpServer;
  */
 public class ProviderExample {
     public static void main(String[] args) {
+
+        // 注册服务
+        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
+
+        // 启动web服务
         HttpServer httpServer = new VertxHttpServer();
         httpServer.doStart(8080);
     }
